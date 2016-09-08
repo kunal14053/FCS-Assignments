ac59f7d0400b502618d4d105fa1186cc0ffd35ba
kunal14053@iiitd.ac.in <kunal14053@iiitd.ac.in>
2016-09-08 15:07:27 +0530

    Assignment 1 Q-3 REDO

--- Assignment1/FeistelCipher.java
+++ Assignment1/FeistelCipher.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by KunalSaini on 05-Sep-16.
@@ -33,25 +35,15 @@ public class FeistelCipher {
 
     public static String ConvertToBinary(String input)
     {
-        String result = "";
-        char[] InputArray = input.toCharArray();
-        for (int i = 0; i < InputArray.length; i++) {
-            result += Integer.toBinaryString(InputArray[i]);
-        }
-        return result;
+        String result = new BigInteger(input.getBytes()).toString(2);
+        //System.out.println("Binary String: "+'0'+result);
+        return ('0'+result);
     }
 
     public static String ConvertToAscii(String s)
     {
-        String s2 = "";
-        char nextChar;
-
-        for(int i = 0; i <= s.length()-8; i += 9) //this is a little tricky.  we want [0, 7], [9, 16], etc (increment index by 9 if bytes are space-delimited)
-        {
-            nextChar = (char)Integer.parseInt(s.substring(i, i+8), 2);
-            s2 += nextChar;
-        }
-        return s2;
+        BigInteger bin=new BigInteger(s,2);
+        return(new String(bin.toByteArray()));
     }
 
 
@@ -65,11 +57,11 @@ public class FeistelCipher {
             {
                 out=out+input.substring(i-4,i)+input.charAt(i-4)+input.charAt(i-3);
             }
-        System.out.println("Expanded : ");
-        System.out.println(out);
+        //System.out.println("Expanded : ");
+        //System.out.println(out);
         //XOR of Key and Expanded input
         String xor=Do_XOR(out,key);
-        System.out.println("XOR key and input : "+xor);
+        //System.out.println("XOR key and input : "+xor);
         //Substitution(8 Blocks - 6 bits to 4 bits each)
         String s1=xor.substring(0,6).substring(1)+xor.substring(0,6).substring(0,1);
         String s2=xor.substring(6,12).substring(1)+xor.substring(6,12).substring(0,1);
@@ -90,7 +82,7 @@ public class FeistelCipher {
         String p8=""+s1.charAt(0)+s1.charAt(2)+s1.charAt(4)+s1.charAt(1);;
         //returning that 32 bit
         String p=p1+p2+p3+p4+p5+p6+p7+p8;
-        System.out.println("Permuted 32 bit: "+p);
+        //System.out.println("Permuted 32 bit: "+p);
         return p;
     }
 
@@ -100,13 +92,13 @@ public class FeistelCipher {
         System.out.println(input);
         String string1=input.substring(0, 32);
         String string2=input.substring(32, 64);
-        System.out.println("L0: "+string1);
-        System.out.println("R0: "+string2);
-        System.out.println("Round1:");
+        //System.out.println("L0: "+string1);
+        //System.out.println("R0: "+string2);
+        //System.out.println("Round1:");
         String string3=FeistelCipher.F_function(string2);
-        System.out.println("L1: "+string2);
-        System.out.println("R1: "+Do_XOR(string1,string3));
-        System.out.println("Cypher Output: "+string2+Do_XOR(string1,string3));
+        //System.out.println("L1: "+string2);
+        //System.out.println("R1: "+Do_XOR(string1,string3));
+        //System.out.println("Cypher Output: "+string2+Do_XOR(string1,string3));
         System.out.println("Cypher Output in Ascii:"+FeistelCipher.ConvertToAscii(string2+Do_XOR(string1,string3)));
         return (string2+Do_XOR(string1,string3));
     }
@@ -120,48 +112,41 @@ public class FeistelCipher {
         System.out.println("Please Specify the input string:\n");
         String plaintext=br.readLine();
         String BinaryInput=FeistelCipher.ConvertToBinary(plaintext);
+        String s;
+        int l=64-BinaryInput.length()%64;
+        String padding="";
+        for(int i=0;i<l;i++)
+            padding=padding+"0";
+        BinaryInput=BinaryInput+padding;
         System.out.println(BinaryInput);
-        ArrayList<String>Blocks=new ArrayList<String>();
-        char[] InputArray = BinaryInput.toCharArray();
-        String s="";
-        int count=0;
-        for (int i = 0; i < InputArray.length; i++) {
-            s=s+InputArray[i];
-            if(i%64==0)
-            {
-                Blocks.add(count,s);
-                s="";
-                count++;
-            }
-            if(i==InputArray.length-1 && i%64!=0)
-            {
-                System.out.println("Padding with zeros: ");
-                for(int j=s.length();j<64;j++)
-                    s=s+'0';
-                Blocks.add(count,s);
-            }
-        }
+        for (int i = 64; i < BinaryInput.length()+64; i=i+64) {
 
-        for(int i=1;i<Blocks.size();i++) {
+          s="";
+            s = BinaryInput.substring(i-64, i);
             System.out.println("Cyper:");
-            String cyper=FeistelCipher.Feistel(Blocks.get(i).toString());
+            String cyper=FeistelCipher.Feistel(s);
             System.out.println("Decypher:");
             FeistelCipher.Decypher(cyper);
+
         }
+
+
+
+
     }
 
 
     private static void Decypher(String cyper) throws IOException {
-        System.out.println("First Round: ");
+        //System.out.println("First Round: ");
         String L1=cyper.substring(0,32);
         String R1=cyper.substring(32,64);
-        System.out.println("L1: "+L1);
-        System.out.println("R1: "+R1);
+        //System.out.println("L1: "+L1);
+        //System.out.println("R1: "+R1);
         String L0=Do_XOR(R1,FeistelCipher.F_function(L1));
         String R0=L1;
-        System.out.println("L0: "+L0);
-        System.out.println("R0: "+R0);
-        System.out.println("Decypher OutPut:"+L0+R0);
+        //System.out.println("L0: "+L0);
+        //System.out.println("R0: "+R0);
+        //System.out.println("Decypher OutPut:"+L0+R0);
         System.out.println("Decypher Output in Ascii:"+FeistelCipher.ConvertToAscii(L0+R0));
     }
 
